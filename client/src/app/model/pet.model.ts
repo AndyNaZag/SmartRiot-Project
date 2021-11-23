aa2da3f5d68ee70f201a246646196aab85ed1f83
AndyNaZag <60372794+AndyNaZag@users.noreply.github.com>
2021-11-22 22:35:40 -0500

    test1

--- client/src/app/model/pet.model.ts
+++ client/src/app/model/pet.model.ts
@@ -1,18 +1,8 @@
 export class Pet {
     constructor(
-        public _id?: number,
+        public id?: number,
         public name?: string,
         public category?: string,
         public description?: string,
         public age?: number) { }
-    
-        public toString(): string{
-            return `Pet
-            -------------------------------
-            Name       : ${this.name}
-            Author     : ${this.category}
-            Description: ${this.description}
-            Price      : ${this.age}
-            -------------------------------`;
-        }
     }
